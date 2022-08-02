072cedf0d9b261300cde5f34dae44b546f2f4dfd
pedroygor <pedroygor2010@hotmail.com>
2022-08-02 17:55:49 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
@@ -7,6 +8,19 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <h1>{title}</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link data-testid="link-to-favorites" to="/favorites"> Favoritos </Link>
+            </li>
+            <li>
+              <Link data-testid="link-to-profile" to="/profile"> Profile </Link>
+            </li>
+            <li>
+              <Link data-testid="link-to-search" to="/search"> Search </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
